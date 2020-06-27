d03ef11dc66389e7d86a97dd945117ac8967489a
MatthewSDick <matthew.dick@outlook.com>
2020-06-26 23:31:13 -0400

    Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Toolbar from "./components/Toolbar/Toolbar";
+import SideDrawer from "./components/SideDrawer/SideDrawer";
+import Backdrop from "./components/Backdrop/Backdrop";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  state = {
+    sideDrawerOpen: false,
+  };
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen };
+    });
+  };
+
+  backdropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false });
+  };
+
+  render() {
+    // let sideDrawer;
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      // sideDrawer = <SideDrawer />;
+      backdrop = <Backdrop click={this.backdropClickHandler} />;
+    }
+    return (
+      <div style={{ height: "100%" }}>
+        <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
+        <SideDrawer show={this.state.sideDrawerOpen} />
+        {/* {sideDrawer} */}
+        {backdrop}
+        <main style={{ marginTop: "64px" }}>{/* <p>This   is the page content!</p> */}</main>
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,9 +46,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
+      </header> */}
+      </div>
+    );
+  }
 }
 
 export default App;
